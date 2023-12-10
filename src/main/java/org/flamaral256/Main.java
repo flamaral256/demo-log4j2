fc55c5efef3e8f7eac4f8ff6e5d4b878d16ab70f
Fernando Amaral <fernandojunior@luizalabs.com>
2023-12-10 13:19:45 -0300

    upgrade para junit5 + jul config in test

--- src/main/java/org/flamaral256/Main.java
+++ src/main/java/org/flamaral256/Main.java
@@ -4,18 +4,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.LogManager;
 
+import static java.util.Objects.requireNonNull;
+
 public class Main {
 
-    public static void main(String[] args) {
+    static {
         if (!System.getProperties().containsKey("java.util.logging.config.file")) {
             try (InputStream julConfigFile = Main.class.getResourceAsStream("/logging.properties")) {
+                requireNonNull(julConfigFile, "logging.properties not founded in classpath");
                 LogManager.getLogManager().readConfiguration(julConfigFile);
             } catch (IOException e) {
                 e.printStackTrace();
                 System.exit(-1);
             }
         }
+    }
 
+    public static void main(String[] args) {
         final Application app = new Application();
         app.run();
         System.exit(0);
--- src/test/java/org/flamaral256/ApplicationTests.java
+++ src/test/java/org/flamaral256/ApplicationTests.java
@@ -1,18 +1,40 @@
 package org.flamaral256;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Test;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTests {
 
-	private static final Logger logger = LogManager.getLogger(ApplicationTests.class);
+    static {
+        if (!System.getProperties().containsKey("java.util.logging.config.file")) {
+            try (InputStream julConfigFile = Main.class.getResourceAsStream("/logging.properties")) {
+                requireNonNull(julConfigFile, "logging.properties not founded in classpath");
+                LogManager.getLogManager().readConfiguration(julConfigFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+    }
 
-	@Test
-	public void foo() {
-		logger.info("Logging in a test scope");
-		assertTrue(true);
-	}
+    private static final org.apache.logging.log4j.Logger LOG4J = org.apache.logging.log4j.LogManager.getLogger(ApplicationTests.class);
+    private static final java.util.logging.Logger JUL = java.util.logging.Logger.getLogger(ApplicationTests.class.getName());
+
+    @Test
+    public void logWithJul() {
+        JUL.finest("Logging in a test scope with jul");
+        assertTrue(true);
+    }
+
+    @Test
+    public void logWithLog4j() {
+        LOG4J.info("Logging in a test scope with log4j");
+        assertTrue(true);
+    }
 }
