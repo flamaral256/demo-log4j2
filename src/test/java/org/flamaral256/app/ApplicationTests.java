b630c7508dc9d432870412bba04d56e3ee790644
Fernando Amaral <fernandojunior@luizalabs.com>
2023-12-19 23:39:55 -0300

    padronizacao log formatter

--- src/test/java/org/flamaral256/app/ApplicationTests.java
+++ src/test/java/org/flamaral256/app/ApplicationTests.java
@@ -10,13 +10,13 @@ public class ApplicationTests {
     private static final java.util.logging.Logger JUL = java.util.logging.Logger.getLogger(ApplicationTests.class.getName());
 
     @Test
-    public void logWithJul() {
+    public void log_with_Jul() {
         JUL.finest("Logging in a test scope with jul");
         assertTrue(true);
     }
 
     @Test
-    public void logWithLog4j() {
+    public void log_with_Log4j() {
         LOG4J.info("Logging in a test scope with log4j");
         assertTrue(true);
     }
