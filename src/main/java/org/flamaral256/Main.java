e5188102cb16dfb3ce52f053c8644c7797f8cb7c
Fernando Amaral <fernandojunior@luizalabs.com>
2025-02-01 18:29:56 -0300

    ajuste na config do jul

--- src/main/java/org/flamaral256/Main.java
+++ src/main/java/org/flamaral256/Main.java
@@ -4,7 +4,9 @@ import org.flamaral256.app.Application;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import static java.util.Objects.requireNonNull;
 
@@ -13,13 +15,13 @@ public class Main {
     public static void configureJul() {
         System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager"); // routes JUL to log4j
 
-        // if a -D parameter was not defined with a logging.properties at java -jar, use the default file in resources
+        // if a -D parameter was not defined with a logging.propertie at java -jar command, use the default file in resources
         if (!System.getProperties().containsKey("java.util.logging.config.file")) {
             try (InputStream julConfigFile = Main.class.getResourceAsStream("/logging.properties")) {
                 requireNonNull(julConfigFile, "logging.properties not founded in classpath");
                 LogManager.getLogManager().readConfiguration(julConfigFile);
             } catch (IOException e) {
-                e.printStackTrace();
+                Logger.getGlobal().log(Level.WARNING, "IOException getting resource logging.properties", e);
                 System.exit(-1);
             }
         }
